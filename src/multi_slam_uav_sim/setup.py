092a9a0ab1655c9ba883a68f88680f57efe8d107
Yicheng-Zhu <2025111580@stu.hit.edu.cn>
2026-07-30 10:21:55 +0800

    feat: add d435i rtabmap visual slam profile

--- src/multi_slam_uav_sim/setup.py
+++ src/multi_slam_uav_sim/setup.py
@@ -14,6 +14,7 @@ data_files = [
     ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
     (f"share/{package_name}", ["package.xml"]),
     (f"share/{package_name}/worlds", glob("worlds/*.sdf") + glob("worlds/*.world")),
+    (f"share/{package_name}/launch", glob("launch/*.launch.py")),
     (f"share/{package_name}/config", glob("config/*")),
     (f"share/{package_name}/params", glob("params/*")),
     (f"share/{package_name}/scripts", glob("scripts/*")),
